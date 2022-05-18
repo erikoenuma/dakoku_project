1b3b9ab17b4895328c4b6129b0a733da65f7cf35
erikoenuma <koenuma.eri@gmail.com>
2022-05-18 15:38:21 +0900

    Revert "Merge pull request #1 from erikoenuma/login"
    This reverts commit 0ac34cf972b4cd926bb27c54fc56fe4370d89259, reversing
    changes made to 113bc4dca83597e38f55485cc512be4c482cb028.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  root "users#sign_in"
-  devise_for :users, controllers: { 
-    sessions: 'users/sessions'
-  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
 end
