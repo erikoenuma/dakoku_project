3d1dc0a525d55ca6192ba043646c1859cdec5a7a
erikoenuma <koenuma.eri@gmail.com>
2022-06-05 15:40:24 +0900

    contract作成時のend_atにstart_atより後の日付になるようバリデーションを追加

--- app/controllers/companies/contracts_controller.rb
+++ app/controllers/companies/contracts_controller.rb
@@ -20,12 +20,14 @@ class Companies::ContractsController < ApplicationController
     @company = Company.find(params[:company_id])
     @project = @company.projects.find(params[:id])
 
-    if @project.user_ids.include?(Integer(params[:user_id]))
-      flash[:alert] = "そのユーザーは既にアサインされています"
+    # 既にアサインされている場合を除く
+    @user_project = UserProject.find_or_create_by(project_id: params[:id], user_id: params[:user_id])
+    if @user_project.contract != nil 
+      flash[:danger] = "そのユーザーは既にアサインされています"
       redirect_to new_assign_employee_company_contracts_path(@company, @project)
       return
     end
-    @user_project = UserProject.find_or_create_by(project_id: params[:id], user_id: params[:user_id])
+
     @contract = @user_project.build_contract(contract_params)
 
     respond_to do |format|
@@ -33,8 +35,9 @@ class Companies::ContractsController < ApplicationController
         flash[:success] = t('.create.success')
         format.html { redirect_to company_project_url(@company, @project) }
       else
-        flash[:success] = t('.create.failure')
-        format.html { redirect_to new_assign_employee_company_contracts_path(@company, @project), status: :unprocessable_entity }
+        # アサイン失敗した場合user_projectも削除する
+        @user_project.destroy
+        format.html { render :new_assign_employee }
       end  
     end  
   end
@@ -44,27 +47,38 @@ class Companies::ContractsController < ApplicationController
     @company = Company.find(params[:company_id])
     @project = @company.projects.find(params[:id])
     @user = User.where(email: params[:contract][:email]).first
+
     if @user.nil?
-      redirect_to new_assign_not_employee_company_contracts_path(@company, @project), alert: "そのユーザーは存在しません。メールアドレスを確認してください。"
+      flash[:danger] = "そのユーザーは存在しません。メールアドレスを確認してください。"
+      redirect_to new_assign_not_employee_company_contracts_path(@company, @project)
+      return
     elsif @user.company_user
-      redirect_to new_assign_not_employee_company_contracts_path(@company, @project), alert: "企業に属しているためアサインできません"
-    elsif @project.users.include?(@user)
-      redirect_to new_assign_not_employee_company_contracts_path(@company, @project), alert: "そのユーザーは既にアサインされています"
-    else 
+      flash[:danger] = "そのユーザーは企業に属しているためアサインできません"
+      redirect_to new_assign_not_employee_company_contracts_path(@company, @project)
+      return
+    end 
 
-      @user_project = UserProject.find_or_create_by(project_id: params[:id], user_id: @user.id)
-      @contract = @user_project.build_contract(contract_params)
-
-      respond_to do |format|
-        if @contract.save
-          flash[:success] = t('.create.success')
-          format.html { redirect_to company_project_url(@company, @project) }
-        else
-          flash[:danger] = t('.create.failure')
-          format.html { redirect_to new_assign_not_employee_company_contracts_path(@company, @project), status: :unprocessable_entity }
-        end  
-      end    
+    @user_project = UserProject.find_or_create_by(project_id: params[:id], user_id: @user.id)
+    
+    if @user_project.contract != nil
+      flash[:danger] = "そのユーザーは既にアサインされています"
+      redirect_to new_assign_not_employee_company_contracts_path(@company, @project)
+      return
     end
+
+    @contract = @user_project.build_contract(contract_params)
+
+    respond_to do |format|
+      if @contract.save
+        flash[:success] = t('.create.success')
+        format.html { redirect_to company_project_url(@company, @project) }
+      else
+        # アサイン失敗した場合user_projectも削除する
+        @user_project.destroy
+        format.html { render :new_assign_not_employee }
+      end  
+    end
+
   end
 
   # PATCH/PUT /contracts/1
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,9 +1,42 @@
 class Contract < ApplicationRecord
   validates :wage_per, length: { maximum: 255 }
   validates :start_at, presence: true
+  validate :end_at_before_start_at
+  # validate :not_assigned_before
   validates :daily_reports_required, inclusion: { in: [true, false] }
   validates :role, presence: true, length: { maximum:255 }
   validates :under_contract, inclusion: { in: [true, false] }
 
   belongs_to :user_project
+
+  # validation
+
+  # 終了日時は開始日時より前
+  def end_at_before_start_at
+    unless self.start_at < self.end_at then
+      errors.add(:end_at, "はアサイン開始日より後の日付けになるようにしてください。")
+    end
+  end
+
+  # # 二重にアサインされないようにする
+  # def not_assigned_before
+  #   @project = Project.find(params[:id])
+
+  #   if params[:user_id].nil?
+  #     @user = User.where(email: params[:contract][:email])
+  #     if @project.users.include?(@user)
+  #       errors.add("そのユーザーは既にアサインされています")
+  #     elsif @user.company_user
+  #       errors.add("企業に属しているためアサインできません")
+  #     elsif @user.nil?
+  #       errors.add("そのユーザーは存在しません。メールアドレスを確認してください。")
+  #     end
+
+  #   else
+  #     @user = User.where(id:params[:user_id])
+  #     if @project.users.include?(@user)
+  #       errors.add("そのユーザーは既にアサインされています")
+  #     end
+  #   end
+  # end
 end
