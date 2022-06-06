83ee8c17d6d3de4f4eb2f9900b0c534e31409bf6
erikoenuma <koenuma.eri@gmail.com>
2022-06-06 16:29:39 +0900

    契約情報の契約期間のバリデーション修正

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -13,30 +13,8 @@ class Contract < ApplicationRecord
 
   # 終了日時は開始日時より前
   def end_at_before_start_at
-    unless self.start_at < self.end_at then
+    if self.end_at != nil && self.start_at > self.end_at then
       errors.add(:end_at, "はアサイン開始日より後の日付けになるようにしてください。")
     end
   end
-
-  # # 二重にアサインされないようにする
-  # def not_assigned_before
-  #   @project = Project.find(params[:id])
-
-  #   if params[:user_id].nil?
-  #     @user = User.where(email: params[:contract][:email])
-  #     if @project.users.include?(@user)
-  #       errors.add("そのユーザーは既にアサインされています")
-  #     elsif @user.company_user
-  #       errors.add("企業に属しているためアサインできません")
-  #     elsif @user.nil?
-  #       errors.add("そのユーザーは存在しません。メールアドレスを確認してください。")
-  #     end
-
-  #   else
-  #     @user = User.where(id:params[:user_id])
-  #     if @project.users.include?(@user)
-  #       errors.add("そのユーザーは既にアサインされています")
-  #     end
-  #   end
-  # end
 end
