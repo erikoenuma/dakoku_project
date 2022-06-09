db0a446eff1b82a1d903a49323a0f0d86fa9acff
erikoenuma <koenuma.eri@gmail.com>
2022-06-09 15:12:26 +0900

    sendgrid修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     port: 587,
     domain: 'heroku.com',
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_APIKEY'],
+    password: ENV['SENDGRID_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true
 }
