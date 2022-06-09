ac94842d7597bc80cf1ab66e3cb6c5971166242b
erikoenuma <koenuma.eri@gmail.com>
2022-06-09 15:39:44 +0900

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "freelance_dakoku_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'https://www.glacial-garden-63679.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://glacial-garden-63679.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   # deliverメソッドを呼んだらメールが送信される
   config.action_mailer.perform_deliveries = true
