e7723ac63c5bee63393a132fdb51f06f362c72dd
erikoenuma <koenuma.eri@gmail.com>
2022-06-09 17:32:28 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { 
     :sessions => 'users/sessions',
-    :registrations => 'users/registrations'
+    :registrations => 'users/registrations',
     :passwords => 'users/passwords'
   }
 
